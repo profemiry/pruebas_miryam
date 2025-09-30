c660e81710ac36d07c83f397816c22cfd5dcc11f
profemiry <miryam.jimenez@iespoligonosur.org>
2025-09-30 11:55:05 +0000

    Subida documentación

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"prueba","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"prueba","l":"prueba()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ prueba.java
@@ -0,0 +1,14 @@
+/**
+ * Programa que muestra el resultado de una multiplicación.
+ * Sirve como ejemplo básico para practicar comentarios JavaDoc.
+ *
+ * @author Miryam JM
+ * @version 1.0
+ */
+public class prueba {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        System.out.println("Resultado: " + (a * b));
+    }
+}
